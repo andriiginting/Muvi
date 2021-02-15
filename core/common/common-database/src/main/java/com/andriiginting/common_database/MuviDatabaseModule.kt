b41e4ff552fe8f220da8576102b8490dc9ceda7b
andriiginting <andris.ginting@gmail.com>
2021-02-15 23:42:48 +0700

    [Andri] Add database encryption

--- core/common/common-database/src/main/java/com/andriiginting/common_database/MuviDatabaseModule.kt
+++ core/common/common-database/src/main/java/com/andriiginting/common_database/MuviDatabaseModule.kt
@@ -5,6 +5,8 @@ import androidx.room.Room
 import com.andriiginting.common_database.Constants.FAVORITE_DATABASE_NAME
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -13,9 +15,13 @@ class MuviDatabaseModule {
     @Provides
     @Singleton
     fun provideDatabase(context: Context): MuviDatabase {
+        val factory = SupportFactory(
+            SQLiteDatabase.getBytes("FAVORITE_DATABASE_NAME".toCharArray())
+        )
         return Room
             .databaseBuilder(context, MuviDatabase::class.java, FAVORITE_DATABASE_NAME)
             .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 }
